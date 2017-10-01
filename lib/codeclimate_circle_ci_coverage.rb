a5272516817ef5a21425949d9e5f5d189395ab68
Robin Dunlop <robin@dunlopweb.com>
2017-09-30 19:55:05 -0500

    add Circle CI 2.0 support, and use new CodeClimateReporter

--- lib/codeclimate_circle_ci_coverage.rb
+++ lib/codeclimate_circle_ci_coverage.rb
@@ -1,3 +1,5 @@
 require "codeclimate_circle_ci_coverage/version"
 require "codeclimate_circle_ci_coverage/patch_simplecov"
 require "codeclimate_circle_ci_coverage/coverage_reporter"
+require "codeclimate_circle_ci_coverage/circle_ci_1"
+require "codeclimate_circle_ci_coverage/circle_ci_2"
